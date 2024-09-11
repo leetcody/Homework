24330df20c8c7f2f152be232a9066d1106ed26d0
leetcody <leetcody@icloud.com>
2024-09-11 12:38:29 -0300

    Create homework01.cs

---
+++ homework01.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp1;
+
+internal class Program { 
+    private static void Main() {
+        string firstName = "Cody";
+        string lastName = "Leet";
+        string country = "Canada";
+        string college = "NBCC";
+        string programOfStudy = "IT: Software Development";
+        
+        string outputString = "My name is " + firstName + " " + lastName + ", and I am from" + country + ".\n" + 
+                              "I am currently enrolled in " + programOfStudy + " at " + college;
+        
+        Console.WriteLine(outputString);
+    }
+}
